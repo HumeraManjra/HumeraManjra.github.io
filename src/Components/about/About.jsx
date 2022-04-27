6e9fffa76c64703201a8992c0d3b2045b7085d54
HumeraManjra <humera.1@hotmail.com>
2022-04-27 19:56:28 -0400

    css and paragraph text updated

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -4,25 +4,24 @@ import SocialFollow from './SocialFollow'
 
 function About() {
   return (
-    <section id='about' className='about__section'>
+    <section id='about' className='about__section container'>
     <h2>ABOUT ME</h2>
     <article className='container about__container'>
         <p>
-              {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
-              esse nihil, non facilis dolore itaque nobis reiciendis minima magnam
-              ea ipsa aut atque temporibus assumenda eos omnis vel voluptatem
-              architecto! Lorem ipsum dolor sit, amet consectetur adipisicing
-              elit. Deserunt esse nihil, non facilis dolore itaque nobis
-              reiciendis minima magnam ea ipsa aut atque temporibus assumenda eos
-              omnis vel voluptatem architecto!  Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Deserunt esse nihil, non facilis
-              dolore itaque nobis reiciendis minima magnam ea ipsa aut atque
-              temporibus assumenda eos omnis vel voluptatem architecto! Lorem
-              ipsum dolor sit, amet consectetur adipisicing elit. Deserunt esse
-              nihil, non facilis dolore itaque nobis reiciendis minima magnam ea
-              ipsa aut atque temporibus assumenda eos omnis vel voluptatem
-              architecto! */}
-            </p>
+        Hello World! I'm Humers Manjra, a Front-End Web Developer based out of Toronto. 
+        From bussiness field to lines of code on a web page, 
+        I love combining the worlds of logic and creative design to make eye-catching, 
+        accessible, and user-friendly websites and applications.
+        </p>
+        <p>
+          Technology leads, society follows. 
+          The move towards increasing inclusivity and diversity in the industry through 
+          representation is of importance to me.
+        </p>
+        <p> 
+            I'm excited to make the leap and continue refining my skills with the right company. 
+            Drop me a line and/or your favourite restaurant in Toronto in the contact form below or check me out at:
+        </p>
             <SocialFollow/>
       </article>
     </section>
