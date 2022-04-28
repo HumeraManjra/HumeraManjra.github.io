b43f2aab7e5ff3c41ea3761d0828e22d1f3d2b8a
HumeraManjra <humera.1@hotmail.com>
2022-04-27 20:02:42 -0400

    projects information removed and created separated components for project information

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -1,35 +1,15 @@
 import React from 'react'
-import InfoCard from './InfoCard'
-import sajjboutiqeCover from '../../assets/sajjboutiqeCover.jpg';
-import mackbook from '../../assets/macbook.jpg';
 import './projects.css'
+import ProjectOne from './ProjectOne'
+import ProjectTwo from './ProjectTwo'
+
 
 function Projects() {
   return (
-    <section id='projects' className='project__section'>
+    <section id='projects' className='project__section container'>
       <h2>Projects</h2>
-
-      <div className="wrapper">
-      <article>
-        <img src={sajjboutiqeCover} alt="" />
-      </article>
-     <InfoCard/>
-      </div>
-
-      <div className="wrapper">
-     <InfoCard/>
-     <article>
-        <img src={mackbook} alt="" />
-      </article>
-      </div>
-
-      
-      <div className="wrapper">
-      <article>
-        <img src={sajjboutiqeCover} alt="" />
-      </article>
-     <InfoCard/>
-      </div>
+      <ProjectOne/>
+      <ProjectTwo/>
     </section>
   )
 }
