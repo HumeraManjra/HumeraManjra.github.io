61f61938aa8519d1c704e1f9fcde3e8f1a68d1f0
HumeraManjra <humera.1@hotmail.com>
2022-10-26 21:08:30 -0400

    projects info updated

--- src/Components/projects/Data.jsx
+++ src/Components/projects/Data.jsx
@@ -1,5 +1,4 @@
 import SajjBoutique from "../../assets/SajjMockup.png";
-import MIRECIPEBOOK from "../../assets/RecipeMockup.png";
 import CreamyCreations from "../../assets/CreamyCreationMockup.png";
 import { DiHtml5, DiCss3, DiReact, DiSass } from "react-icons/di";
 
@@ -9,28 +8,38 @@ let Data = [
     img: SajjBoutique,
     heading: "SajjBoutique",
     Description:
-      "SajjBoutique is a responsive e-commerce web application for a local clothing store",
+      "SajjBoutique is a responsive web application for a local clothing store",
     Icons: [DiHtml5, DiSass, DiReact],
     URL: "https://sajjboutique.ca/",
   },
+
   {
     id: "002",
-    img: MIRECIPEBOOK,
-    heading: "MIRECIPEBOOK",
-    Description:
-      "MIRECIPEBOOK is a responsive web application for publishing and browsing recipes",
-    Icons: [DiHtml5, DiCss3, DiReact],
-    URL: "https://sajjboutique.ca/",
-  },
-  {
-    id: "003",
     img: CreamyCreations,
     heading: "Creamy Creations",
     Description:
       "Creamy Creations is a responsive web application for Cake Shop",
     Icons: [DiHtml5, DiCss3, DiReact],
-    URL: "https://sajjboutique.ca/",
+    URL: "https://creamycreations.ca/",
   },
+  // {
+  //   id: "003",
+  //   img: "Coming soon...",
+  //   heading: "Pharmasave Speers Pharmacy",
+  //   Description: "Pharmasave Speers Pharmacy is a responsive web application for filling online prescription, booking appointment, view services and more",
+  //   Icons: [DiHtml5, DiCss3, DiReact],
+  //   URL: " ",
+  // },
+
+  // {
+  //   id: "004",
+  //   img: "Coming soon...",
+  //   heading: "MIRECIPEBOOK",
+  //   Description:
+  //     "MIRECIPEBOOK is a responsive web application for publishing and browsing recipes",
+  //   Icons: [DiHtml5, DiCss3, DiReact],
+  //   URL: " ",
+  // },
 ];
 
 export default Data;
